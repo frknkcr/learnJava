9de574e5b9df9451019c82846b834a8de789d295
Muhammed Furkan Kocer <muhammedfurkankocer@gmail.com>
2023-01-16 00:57:42 +0300

    v9

--- src/ders38_maps_nestedMaps/C03_MapMethodlari.java
+++ src/ders38_maps_nestedMaps/C03_MapMethodlari.java
@@ -10,6 +10,26 @@ public class C03_MapMethodlari {
 
         Map<Integer,String> ogrenciMap = MapMethodDepo.ogrenciMapOlustur();
 
+        System.out.println(ogrenciMap.containsValue("Ali")); // false
+        // value degerinin tamami sorulursa true, bir parcasi sorulursa false doner
+
+        System.out.println(ogrenciMap.getOrDefault(103, "Ogrenci bulunamadi"));
+        // Ali-Cem-11-K-TM
+
+        System.out.println(ogrenciMap.getOrDefault(123,"Ogrenci bulunamadi"));
+        // Ogrenci bulunamadi
+
+        System.out.println(ogrenciMap.get(123)); // null
+
+        ogrenciMap.replace(103,"Ali-Cem-11-K-TM");
+
+        System.out.println(ogrenciMap.get(103)); // Ali-Cem-11-K-TM
+
+        ogrenciMap.put(103,"Ali-Cem-12-K-TM");
+
+        System.out.println(ogrenciMap.get(103)); // Ali-Cem-12-K-TM
+
+
 
 
 

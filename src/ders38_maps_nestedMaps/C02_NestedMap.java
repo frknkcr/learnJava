8780bca84e5544eb1ddb2793996de665077081c4
Muhammed Furkan Kocer <muhammedfurkankocer@gmail.com>
2023-01-15 23:04:20 +0300

    v8

--- src/ders38_maps_nestedMaps/C02_NestedMap.java
+++ src/ders38_maps_nestedMaps/C02_NestedMap.java
@@ -1,13 +1,15 @@
 package ders38_maps_nestedMaps;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class C02_NestedMap {
 
     public static void main(String[] args) {
 
         /*
-
+        Asagidaki body'i map olarak olusturun
         {
         "firstname" : "Ahmet",
         "lastname" : "Bulut",
@@ -21,9 +23,39 @@ public class C02_NestedMap {
 
          */
 
+        Map<String,String> bookingdatesMap = new HashMap<>();
+        bookingdatesMap.put("checkin" , "2021-06-01");
+        bookingdatesMap.put("checkout" , "2021-06-10");
+
+        Map<String, Object> bookingMap = new HashMap<>();
+        bookingMap.put("firstname" , "Ahmet");
+        bookingMap.put("lastname" , "Bulut");
+        bookingMap.put("totalprice" , 500);
+        bookingMap.put("bookingdates" , bookingdatesMap);
+        bookingMap.put("additionalneeds" , "wi-fi");
+
+        System.out.println(bookingMap);
+
+        /*
+        {firstname=Ahmet, additionalneeds=wi-fi, bookingdates={checkin=2021-06-01, checkout=2021-06-10}, totalprice=500, lastname=Bulut}
+         */
+
+        System.out.println(bookingMap.get("firstname")); // Ahmet
+
+        System.out.println(bookingMap.get("additionalneeds")); // wi-fi
+
+        // check-in bilgisini yazdirin
+        // booking map'indeki value data turu Object sectik
+        // dolayisiyla bookingdates key'ine ait value'yu Object olarak bize getirir
+        // ama biz onun map oldugunu biliyoruz
+        // casting yapabiliriz
 
 
+        System.out.println(((Map<?, ?>) bookingMap.get("bookingdates")).get("checkin")); // 2021-06-01
 
+        // check-out tarihini yazdiralim
+
+        System.out.println(((Map<?, ?>) bookingMap.get("bookingdates")).get("checkout")); // 2021-06-10
     }
 
 }
